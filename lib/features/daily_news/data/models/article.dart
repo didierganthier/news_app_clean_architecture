2da351e075f934ffe5b71736d628612fe643ebac
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-08 13:22:12 -0500

    Add Article model and repository implementation for news articles

---
+++ lib/features/daily_news/data/models/article.dart
@@ -0,0 +1,36 @@
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+
+class ArticleModel extends ArticleEntity {
+  final int? id;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  ArticleModel({
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      id: json['id'],
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: json['publishedAt'],
+      content: json['content'],
+    );
+  }
+}

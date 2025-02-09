0a6948537dcb1e230a6b02d9803b411b5eea721c
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-09 10:55:23 -0500

    Implement ArticleRepository to fetch news articles using NewsApiService

--- lib/features/core/constants/constants.dart
+++ lib/features/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseUrl = 'https://newsapi.org/v2';
+const String newsAPIKey = '0fa22d1b65d349d0a32a62c16de0c5ad';
+const String countryQuery = 'us';
+const String categoryQuery = 'general ';
--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,12 +1,40 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:news_app_clean_architecture/features/core/constants/constants.dart';
 import 'package:news_app_clean_architecture/features/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/models/article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticle() {
-    // TODO: implement getNewsArticle
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticle() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticle(
+        apiKey: newsAPIKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioException(
+            response: httpResponse.response,
+            error: httpResponse.response.statusMessage,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
+    } on DioException catch (error) {
+      return DataFailed(error);
+    }
   }
 }

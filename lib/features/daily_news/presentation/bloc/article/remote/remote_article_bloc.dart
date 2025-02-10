48b2afe1f3e9da1e1991b27e26f380a860e812a1
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-10 12:05:38 -0500

    Implement RemoteArticlesBloc to manage article fetching and state transitions

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:news_app_clean_architecture/features/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+class RemoteArticlesBloc extends Bloc<RemoteArticlesEvent, RemoteArticleState> {
+  final GetArticleUseCase _getArticleUseCase;
+  RemoteArticlesBloc(this._getArticleUseCase)
+      : super(const RemoteArticlesLoading()) {
+    on<GetArticles>(onGetArticles);
+  }
+
+  void onGetArticles(
+      GetArticles event, Emitter<RemoteArticleState> emit) async {
+    final dataState = await _getArticleUseCase(null);
+
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticlesDone(articles: dataState.data!));
+    } else {
+      emit(RemoteArticlesError(dioException: dataState as DioException));
+    }
+  }
+}
